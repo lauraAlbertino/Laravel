69f97ca6a1041d1fb8b1730f855427d0c2161c67
Laura <laura@4medic.com.br>
2026-07-24 12:05:05 -0300

    criacao lista ususarios

--- resources/views/admin/users/partials/form.blade.php
+++ resources/views/admin/users/partials/form.blade.php
@@ -1,13 +1,13 @@
-<x-alert />
+﻿<x-alert />
 
 <div class="">
     @csrf()
     <div class="mb-5">
-        <input type="text" name="name" placeholder="Nome" value="{{ $user->name ?? old('name') }}"
+        <input type="text" name="name" placeholder="Nome" value="{{ old('name', optional($user ?? null)->name) }}"
             class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     </div>
     <div class="mb-5">
-        <input type="email" name="email" placeholder="E-mail"value="{{ $user->email ?? old('email') }}"
+        <input type="email" name="email" placeholder="E-mail" value="{{ old('email', optional($user ?? null)->email) }}"
             class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     </div>
     <div class="mb-5">
@@ -18,3 +18,4 @@
         Enviar
     </button>
 </div>
+
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
-<?php
+﻿<?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/users', UserController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
